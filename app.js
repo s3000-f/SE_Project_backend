eb81583524dd84ce9137d102dc943a648db12ab7
Soroush Fathi <s3000@yahoo.com>
2020-05-15 14:04:17 +0430

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+let express = require('express');
+const createError = require('http-errors');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+const cors = require('cors');
+const config = require("config");
+const databtase = require("./database");
+const indexRouter = require('./routes/index');
+const examsRouter = require('./routes/exam');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+app.use('/api/exams', examsRouter);
+app.use('/api/users', usersRouter);
+
+if (!config.get("myprivatekey")) {
+  console.error("FATAL ERROR: Private Key is not defined.");
+  process.exit(1);
+}
+console.log(databtase);
+
+app.use(function(req, res, next) {
+  console.log("here")
+  next(createError(404));
+});
+
+app.use('/*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.sendFile(__dirname + '/public/index.html');
+
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.send('error');
+});
+module.exports = app;
---
+++ database.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose')
+
+// const server_address = '127.0.0.1:27017'
+// const database_name = 'ieng_final'
+const uri = "mongodb+srv://s3000:Topol1997@exam-r5vro.mongodb.net/test?retryWrites=true&w=majority";
+
+class Database {
+    constructor() {
+        this._connect()
+    }
+
+    // mongoose.connect(`mongodb://${server_address}/${database_name}`, { useNewUrlParser: true, useUnifiedTopology: true})
+
+    _connect() {
+        console.log("connecting to db...");
+        mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+            //     mongoose.connect(`mongodb://${server_address}/${database_name}`, { useNewUrlParser: true, useUnifiedTopology: true})
+            .then(() => {
+                console.log('Database connection made.')
+            })
+            .catch(err => {
+                console.error('Database connection refused.')
+                console.log(err.reason)
+            })
+    }
+}
+
+module.exports = new Database()
+// const MongoClient = require('mongodb').MongoClient;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   mongoose.
+//   // perform actions on the collection object
+//   client.close();
+// });
